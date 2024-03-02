e1270a3149bd0464a1bbd72814bb6daf484ae0b6
Stephan Schulte <stephan.schulte@gmail.com>
2024-03-02 14:48:51 +0100

    initial

---
+++ pico/ASM/Books/01_HelloWorld/build/CMakeFiles/HelloWorld.dir/home/stephan/apps/pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.obj.d
@@ -0,0 +1,30 @@
+CMakeFiles/HelloWorld.dir/home/stephan/apps/pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.obj: \
+ /home/stephan/apps/pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/alloca.h \
+ /home/stephan/apps/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/stephan/apps/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/stephan/apps/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /home/stephan/Repositories/embeddedsystems/pico/ASM/Books/01_HelloWorld/build/generated/pico_base/pico/version.h \
+ /home/stephan/apps/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/stephan/Repositories/embeddedsystems/pico/ASM/Books/01_HelloWorld/build/generated/pico_base/pico/config_autogen.h \
+ /home/stephan/apps/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/stephan/apps/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/stephan/apps/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/stephan/apps/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/stephan/apps/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/stephan/apps/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/stephan/apps/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/stephan/apps/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/stephan/apps/pico/pico-sdk/src/rp2_common/pico_malloc/include/pico/malloc.h
